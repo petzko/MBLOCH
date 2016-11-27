472e88e11dfac40fb1214bece5eec4041ea2eec3
Longway <longways@gmx.de>
2016-11-27 09:46:05 +0100

    add annotation

--- simulation/TL_voltage.m
+++ simulation/TL_voltage.m
@@ -1,5 +1,8 @@
 function dat = TL_voltage(rlgc,dat)
-%%%%% Begin TRANSMISSION LINE EQUATIONS %%%%%%%%%%%%%
+%%%%% TRANSMISSION LINE EQUATIONS %%%%%%%%%%%%%
+% The voltage source (Vs) with wire impedance (Zin) is replace with its Northon
+% equivalent circuit, which consists of a current source (Vs/Zin) and a
+% resist Zin in parallel.
 
 %Definition of constants
 rlgc.C = 1.5;  %unit: pF/mm
@@ -8,11 +11,15 @@ dat.Zin = 50;  %unit: ohm
 dat.Vs = 9;    %unit: V
 
 %Initial conditions
+%At t=0, the voltage on the line was set to be equal with the source, and
+%no current flows to the line. Due to tunneling of electrons (J) which is 
+%relevant to the position, the voltage as well as current along the line 
+%will be changed until to the equilibrium.
 dat.v_TL(1:end) = dat.Vs; % V(0)    V(1)....   V(M)
 dat.i_TL(1:end) = 0;      % I(1/2)  I(3/2).... I(M+1/2)
 
 %Boundary conditions
-dat.i_TL(end) = 0;
+dat.i_TL(end) = 0; % right side open circuit
 
 %Voltage and current updating
 dat.v_TL(1:end) = 1/(rlgc.C*dat.dx/dat.dt+1/2/dat.Zin)*...
