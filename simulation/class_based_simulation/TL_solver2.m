154783b7fcb9b855f3813d60e5d73147497e3f55
Longway <zlongway@gmail.com>
2017-04-22 19:19:18 +0200

    BC: add influence of J at node 0

--- simulation/class_based_simulation/TL_solver2.m
+++ simulation/class_based_simulation/TL_solver2.m
@@ -54,8 +54,8 @@ classdef TL_solver2 < handle
        function propagate2(obj,J_TL1,J_TL0,t)
        
         obj.v_TLold = obj.v_TL; % store the boundary voltage of previous step      
-        obj.v_TL(1)=(-obj.coeff0fBC*obj.v_TL(1)+(1+obj.coeff0fBC)/2*obj.v_TLold2(1)+obj.V_RFnew-obj.V_RFold...
-                      + (obj.i_TL(1)-obj.i_TLold(1))*obj.width*obj.Zin/(obj.height*1e3))/(1/2-obj.coeff0fBC/2);          
+        obj.v_TL(1)=(obj.coeff0fBC*obj.v_TL(1)-(obj.coeff0fBC-1)/2*obj.v_TLold2(1)+obj.V_RFnew-obj.V_RFold...
+                      -(obj.i_TL(1)-obj.i_TLold(1)+obj.dx*(J_TL1(1)-J_TL0(1))/4)*obj.width*obj.Zin/(obj.height*1e3))/(1/2+obj.coeff0fBC/2);          
         obj.v_TL(2:end-1) = obj.v_TL(2:end-1)+obj.Bcoeff*(obj.i_TL(2:end-1)-obj.i_TL(1:end-2)+obj.dx*((J_TL1(2:end-1)+J_TL0(2:end-1))/2));
         obj.v_TL(end) = obj.v_TL(end)+obj.Bcoeff*(obj.i_TL(end)-obj.i_TL(end-1)+(J_TL1(end)+J_TL0(end))*obj.dx/4)*2;
         obj.v_TLold2 = obj.v_TLold;
